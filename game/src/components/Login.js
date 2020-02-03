577fe351bac6aedfee4fcc509fe9aa89712a0c01
unknown <irisjitomo@yahoo.com>
2020-02-03 13:52:43 -0500

    auth login

---
+++ game/src/components/Login.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const Login = (props) => {
+    const credentials = {
+        username: '',
+        password: ''
+    }
+
+    const [login, setLogin] = useState(credentials)
+
+    const changeHandler = e => {
+        e.persist();
+        setLogin({
+            ...login,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const submitHandler = e => {
+        e.preventDefault()
+        axiosWithAuth()
+        .post('', login) // login endpoint goes here
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            props.history.push() // whatever route we want to go to
+        }) 
+    };
+    return (
+        <div className='container'>
+            <form onSubmit={submitHandler}>
+                <input type='text' name='username' onChange={changeHandler} value={login.username}/>
+                <input type='password' name='password' onChange={changeHandler} value={login.password}/>
+            </form>
+        </div>
+    )
+}
+
+export default Login

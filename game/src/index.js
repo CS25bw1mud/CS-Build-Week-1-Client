48e8bf96b86ab815d67ffac2d83271cc4b2de591
bodhibrdr <julie.gumerman@gmail.com>
2020-02-03 11:22:28 -0700

    jdfskal

--- game/src/index.js
+++ game/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { reducer } from "./store/reducer/";
+import { reducer } from "./store/reducers/";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
--- game/src/store/reducers/index.js
+++ game/src/store/reducers/index.js
@@ -1,5 +1,11 @@
-import axios from "axios"
+import React from "react";
+import axios from "axios";
 
 // start, success, failure
 
 //build out functions
+export const reducer = () => {
+    return (
+        <h1>Haiiiii!</h1>
+    )
+}

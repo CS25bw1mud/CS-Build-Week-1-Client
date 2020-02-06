b1107540084330f245ca7601c9ae032dbf902a43
unknown <irisjitomo@yahoo.com>
2020-02-06 14:54:17 -0500

    data properly being called in map.js

--- game/src/store/actions/index.js
+++ game/src/store/actions/index.js
@@ -4,16 +4,18 @@ export const FETCH_DATA_START = "FETCH_DATA_START"
 export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS"
 export const FETCH_DATA_FAILURE = "FETCH_TRAILS_FAILURE"
 
-export const getData = () => dispatch => {
+export const getData = () => {
+    return (dispatch) => {
     dispatch({ type: FETCH_DATA_START})
     axios.get("https://cs25-lock-industry-dev.herokuapp.com/api/adv/map")
         .then(response => {
-            dispatch({ type: FETCH_DATA_SUCCESS, payload: response})
-            console.log(response)
+            console.log(Object.values(response.data))
+            const roomData = Object.values(response.data)
+            dispatch({ type: FETCH_DATA_SUCCESS, payload: roomData})
         })
         .catch(err => {
             dispatch({ type: FETCH_DATA_FAILURE, payload: `${err}` })
             console.log("cool story bro", err)
         })
-
+    }
 }

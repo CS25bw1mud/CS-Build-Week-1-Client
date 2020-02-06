4e57967f31df85640f7c01f8a144f0a9f3a2a220
System Administrator <jon.s.solari@gmail.com>
2020-02-06 17:25:59 -0500

    :art: map populated
    map populated

--- game/src/components/Map.js
+++ game/src/components/Map.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import './component-css/Map.css';
 import { getData } from "../store/actions";
 import { connect } from 'react-redux'
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, AreaSeries, MarkSeries, HexbinSeries } from 'react-vis';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, AreaSeries, MarkSeries, LineMarkSeries, HexbinSeries } from 'react-vis';
 
 function Map({ data, isFetching, error, getData }) {
 
@@ -354,9 +354,121 @@ console.log('DATA', data)
 return(
     <div class="map-border">
         <XYPlot
-        width={400}
-        height={400}>
+        width={500}
+        height={500}>
         <HorizontalGridLines />
+        <LineMarkSeries
+          data={[//orientation
+            {x: 0, y: 0},
+            //web
+            {x: 0, y: 1},
+            {x: 0, y: 2},
+            {x: 0, y: 3},
+            {x: 0, y: 4},
+            {x: 0, y: 5},
+            {x: 0, y: 6},
+            {x: 0, y: 7},
+            {x: 0, y: 8},
+            {x: 0, y: 9},
+            {x: 0, y: 10},
+            //node
+            {x: -1, y: 10},
+            {x: -1, y: 11},
+            {x: -1, y: 12},
+            {x: -1, y: 13},
+            {x: 0, y: 13},
+            {x: 0, y: 14},
+          ]}
+          lineStyle={{stroke:"red", fill: "none"}}
+        />
+        <LineMarkSeries
+          data={[ //java
+          {x: 0, y: 10},
+          {x: 1, y: 10},
+          {x: 1, y: 11},
+          {x: 1, y: 12},
+          {x: 1, y: 13},
+          {x: 0, y: 13},
+          ]}
+          lineStyle={{stroke:"red", fill: "none"}}
+        />
+        <LineMarkSeries
+          data={[// data science plot
+            {x: 0, y: 0},
+            {x: -1, y: 0},
+            {x: -2, y: 0},
+            {x: -2, y: 1},
+            {x: -2, y: 2},
+            {x: -2, y: 3},
+            {x: -2, y: 4},
+            {x: -2, y: 5},
+            {x: -2, y: 6},
+            {x: -2, y: 7},
+            {x: -2, y: 8},
+            {x: -2, y: 9},
+            {x: -2, y: 10},
+            {x: -2, y: 11},
+            {x: -2, y: 12},
+            {x: -2, y: 13},
+            {x: -2, y: 14},
+            {x: -1, y: 14},
+            {x: 0, y: 14},
+          ]}
+          lineStyle={{stroke:"red", fill:"none"}}
+        />
+        <LineMarkSeries
+        data={[
+          {x:0, y:14},
+          {x:0, y:15},
+          {x:0, y:16},
+          {x:0, y:17},
+          {x:0, y:18},
+          {x:0, y:19},
+          {x:0, y:20}, 
+          {x:0, y:21},
+          {x:0, y:22},
+          {x:0, y:23},
+          {x:0, y:24},
+          {x:0, y:25},
+          {x:0, y:26},
+          {x:0, y:27},
+          {x:0, y:28 }
+        ]}/>
+        <LineMarkSeries
+          data={[// IOS plot
+          {x: 0, y: 0},
+          {x: 1, y: 0},
+          {x: 2, y: 0},
+          {x: 2, y: 1},
+          {x: 2, y: 2},
+          {x: 2, y: 3},
+          {x: 2, y: 4},
+          {x: 2, y: 5},
+          {x: 2, y: 6},
+          {x: 2, y: 7},
+          {x: 2, y: 8},
+          {x: 2, y: 9},
+          {x: 2, y: 10},
+          {x: 2, y: 11},
+          {x: 2, y: 12},
+          {x: 2, y: 13},
+          {x: 2, y: 14},
+          {x: 1, y: 14},
+          {x: 0, y: 14}
+          ]}
+          lineStyle={{stroke:"red", fill:"none"}}
+
+          
+        />
+        <LineMarkSeries
+          data={[// IOS plot
+          {x: 0, y: 0},
+          {x: 0, y: -20}
+          ]}
+          lineStyle={{stroke:"red", fill:"none"}}
+
+          
+        />
         <MarkSeries
         color="darkred"
         data={[

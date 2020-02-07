d44f73f55e5e62edd962c6fac73a435eeb37fbe4
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-02-06 22:20:48 -0500

    WIP: Working on move.

--- game/src/components/Controls.js
+++ game/src/components/Controls.js
@@ -4,25 +4,25 @@ import { getGameInfo } from "../store/actions/index.js";
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 function Controls(props){
 // export default function Controls(){
-        // const [tracker, setTracker] = useState({})
+        const [tracker, setTracker] = useState({})
         useEffect(() => {
             getGameInfo();
             }, [getGameInfo]);
             console.log("Controls", props.gameData)
-    // const movePlayer = (input) => {
-    //     axiosWithAuth()
-    //     .post('https://cs25-lock-industry-dev.herokuapp.com/api/adv/move/', input)
-    //     .then (res => {
-    //         console.log('dir response', res)
-    //         setTracker(res.data)
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-    // }
+    const movePlayer = (input) => {
+        axiosWithAuth()
+        .post('https://cs25-lock-industry-dev.herokuapp.com/api/adv/move/', input)
+        .then(res => {
+            console.log('dir response', res)
+            setTracker(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
     return(
         <div>
-            <button onClick={() => props.gameData({"direction": "n"})}>North</button>
+            <button onClick={() => movePlayer({"direction": "n"})}>North</button>
             <button onClick={() => props.gameData({"direction": "s"})}>South</button>
             <button onClick={() => props.gameData({"direction": "w"})}>West</button>
             <button onClick={() => props.gameData({"direction": "e"})}>East</button>
